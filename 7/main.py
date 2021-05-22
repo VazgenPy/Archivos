3849fc671ac2813f4fb264302bd502c667c54684
vazgeb <>
2021-05-22 21:48:32 +0000

    en 7/main.py

--- 7/main.py
+++ 7/main.py
@@ -1,4 +1,6 @@
+import time
 import os
+import sys
 archivo = open("7/country.csv", "r" , encoding = "utf-8")
 read = archivo.readlines()
 lista = []
@@ -22,10 +24,18 @@ def lectura_de_archivo(file):
 
 
 def UI():
-	print("Bienvenido a mi programa vas a disfrutar de una experiencia bastante educativa, empecemos!")
+	intro = """Bienvenido a mi programa vas a disfrutar de una experiencia bastante educativa
+Controles: ENTER PARA CONTINUAR
+ADELANTE!"""
+	for i in intro:
+		print(i , end='')
+		sys.stdout.flush()
+		time.sleep(0.025)
+	input()
+	cls()
 	opcion = ""
 	while opcion != "5":
-		opcion = input('''1. Search | 2. Continent | 3. Esperanza | 4. Ayuda | 5. Exit
+		opcion = input('''1. Buscar | 2. Continente | 3. Renta y Esperanza | 4. Ayuda | 5. Exit
 ''')
 		if opcion == "1":
 			datos(lista)
@@ -43,59 +53,65 @@ def UI():
 			cls()
 			continue
 		elif opcion == "4":
+			cls()
 			print("""Hola si has entrado aqui es porque necesitas ayuda
 
-1. Search: Esta opcion sirve para buscar los datos basicos de un pais.
+1. Buscar --> Esta opcion sirve para buscar los datos basicos de un pais.
 
-2. Continent: Esta opcion sirve para buscar los paises, la poblacion y la superficie del continente que quieras.
+2. Continente --> Esta opcion sirve para buscar los paises, la poblacion y la superficie del continente que quieras.
 
-3. Esperanza: Esta opcion sirve para buscar el nombre de un pais segun su Renta Per Capita y su Esperanza de Vida.
+3. Renta y Esperanza --> Esta opcion sirve para buscar el nombre de un pais segun su Renta Per Capita y su Esperanza de Vida.
+
+Diviertete :)
 			""")
 			input()
 			cls()
 			continue
 		elif opcion == "5":
-			print("\tAdios amigo mio nos veremos luego!")
+			cls()
+			print("Adios amigo mio nos veremos luego!")
 			exit()
 		else:
 			cls()
 
 
 def datos(lista):
-	a = input("\tDime el nombre del pais: ")
-	b = input("\t\tLo quieres escribir o añadir? ")
+	cls()
+	a = input("Dime el nombre del pais, el Codigo o el nombre Local: ")
+	b = input("\n\tLo quieres escribir o añadir? ")
 	for i in lista:
 		for j in range(len(i)):
 			if i[j] == a:
 				if b == "escribir":
 					for x in range(len(i)):
-						print("\n\t\t\t" + lista[0][x] + ": " + str(i[x]))
+						print("\n\t\t" + lista[0][x] + ": " + str(i[x]))
 					break
 				elif b == "añadir":
 					for w in range(len(i)):
 						arch = open("7/" + i[j] + ".txt", "a" , encoding = "utf-8")
-						arch.write("\n" + lista[0][w] + ": " + str(i[w]) + "\n")
+						arch.write(lista[0][w] + ": " + str(i[w]) + "\n\n")
 						arch.close()
 					break
 			
 
 def continente(lista):
+	cls()
 	population = 0
 	surface = 0
-	a = input("\tDime el continente: ")
-	opcion = input("Que quieres escribir o añadir: ")
+	a = input("Dime el continente: ")
+	opcion = input("\n\tQue quieres escribir o añadir: ")
 	if opcion == "escribir":
 		for i in lista:
 			for j in range(len(i)):
 				try:
 					if i[j] == a:
-						print(i[1])
+						print("\n\t\t" + i[1])
 						population = population + i[6]
 						surface = surface + i[4]
 						break
 				except:
 					continue
-		print("Superficie: " , surface , " habitantes" , "Poblacion: " , population , " Km^2")
+		print("\n\t\tSuperficie: " + str(surface) + " Km^2" + "\n\n\t\tPoblacion: " + str(population) + " habitantes")
 	elif opcion == "añadir":
 		for i in lista:
 			for j in range(len(i)):
@@ -114,15 +130,16 @@ def continente(lista):
 
 
 def esperanza(lista):
+	cls()
 	a = "{:.2f}".format(float(input("Dime la renta per capita: ")))
-	b = float(input("Dime la esperanza de vida: "))
-	c = input("Lo quieres añadir o escribir: ")
+	b = float(input("\n\tDime la esperanza de vida: "))
+	c = input("\n\t\tLo quieres añadir o escribir: ")
 	for i in range(1 , len(lista)):
 		try:
 			if a == "{:.2f}".format(lista[i][8] / lista[i][6]):
 				if b == lista[i][7]:
 					if c == "escribir":	
-						print(lista[i][1] , "," , "{:.2f}".format(lista[i][8] / lista[i][6]) , "," , lista[i][7])
+						print("\n\t\t\t" , lista[i][1] , "\n\n\t\t\t\tRenta per capita: " , "{:.2f}".format(lista[i][8] / lista[i][6]) , "\n\n\t\t\t\t\tEsperanza de vida: " , lista[i][7])
 						break
 					elif c == "añadir":
 						arch = open("7/" + lista[i][1] + "_Esperanza" + ".txt" , "a")
